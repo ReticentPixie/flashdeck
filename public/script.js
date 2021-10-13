45376745e63144c180a03b10aac9abcbda05ead7
Diana Barney <diana.aiken@gmail.com>
2021-10-13 15:02:21 -0400

    fixed bug to implement script.js correctly across app

--- public/script.js
+++ public/script.js
@@ -1,7 +1,12 @@
-
 // use jquery to *flip* the flashcard
-$('#flashcard').on('click', function() {
-    $('#title').fadeOut(500)
-    $('#defintion').fadeIn(500);
+$('#flashcard').click(() => {
+    console.log('made it here')
+    // if titleAttr is undefined then the title it visible
+    if($('#title').attr('hidden') === undefined) {
+      $('#title').fadeOut(1000).attr('hidden', true)
+      $('#definition').fadeIn(1000).removeAttr('hidden')
+    } else {
+      $('#definition').fadeOut(1000).attr('hidden', true)
+      $('#title').fadeIn(1000).removeAttr('hidden')
+    }
 })
-
